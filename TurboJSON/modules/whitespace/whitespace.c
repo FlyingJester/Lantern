a1a9a4ecafce97b21eacc70c41839394b2e9251c
FlyingJester <foolkingcrown@gmail.com>
2016-10-05 10:28:14 -0700

    Use submodules

--- TurboJSON/modules/whitespace/whitespace.c
+++
@@ -1,6 +0,0 @@
-unsigned SkipWhitespace(const char *in, unsigned length){
-    unsigned i = 0;
-    while((*in==' ' || *in=='\t' || *in=='\n') && i++ < length)
-        in++;
-    return i;
-}
--- TurboJSON/modules/whitespace/whitespace.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C"
-#endif
-uint64_t SkipWhitespace(const char *in, uint64_t length);
